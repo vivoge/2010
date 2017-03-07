90492b801747221cc42e4e135b1112feb3235d22
vivoge@163.com <r5n74674kc>
2017-03-07 16:41:33 +0800

    js ti

--- 1.js
+++ 1.js
@@ -1,4 +1,35 @@
-var app = angular.module('app',[]);
-app.controller('mc',['$scope',function($scope){
-    $scope.name = '1';
-}]);
+function fn(n){
+    var arr = [];
+    var max = 23 ;
+    var min = 2;
+
+    function creatNumber(){
+        return parseInt(Math.random()*(max-min+1)+min);
+    }
+
+    for(var i= 0;i<n;i++){
+        var num = creatNumber();
+        if(arr.length == 0){
+            arr.push(num)
+        }
+        if(same(num)){
+            arr.push(num)
+        }else{
+           n-1;
+           continue;
+        }
+    }
+
+    function same(num){
+        for(var i=0;i<arr.length;i++){
+            if(arr[i] == num){
+                return false
+            }
+        }
+        return true
+    }
+
+   return arr;
+}
+
+console.log(fn(8));
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var app = angular.module('app',[]);
 
 
 app.controller('mc',['$scope',function($scope){
-    $scope.name = '1';
-    $scope.aa = function(){
-      console.log('1');
+
+    $scope.aa = function(data){
+      console.log(data);
     };
 
 }]);
+
+
