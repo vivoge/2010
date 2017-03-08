c728698507fd0c483c92c5b92a0264ce0fbfdc0d
vivoge <vivoge@163.com>
2017-03-09 07:50:54 +0800

    03081

--- index.js
+++ index.js
@@ -3,29 +3,37 @@ var app = angular.module('app',[]);
 
 app.controller('mc',['$scope',function($scope){
     $scope.userdata = {};
-    $scope.name = '2';
+
     $scope.aa = function(){
       console.log('提交数据中');
     };
 
 }]);
 
+
 app.directive('compare',function(){
+
     return {
         restrict:'AE',
         require:'ngModel',
-        scope:{
-            orgtext:'=compare'
-        },
+        //scope:{
+        //    orgtext:'=compare',
+        //    org:'=ngModel'
+        //},
         link:function(scope,element,attrs,ngctrl){
             ngctrl.$validators.compare = function(modelValue,viewValue){
                 var val = viewValue || modelValue;
-                return val == scope.orgtext
+                return val == scope.$eval(attrs['compare']);
             };
-            scope.$watch('orgtext',function(){
-                ngctrl.$validate();
-            })
+            //scope.$watch('orgtext',function(){
+            //    ngctrl.$validate();
+            //})
 
+            scope.$watch(function(){
+                return scope.$eval(attrs['compare'])
+            },function(newVal,oldVal){
+                ngctrl.$setValidity('compare',scope.$eval(attrs['compare']) == scope.userdata.rePassword);
+            })
         }
     }
 });
